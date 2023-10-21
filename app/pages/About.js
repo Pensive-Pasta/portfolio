4f1687a5390f9178febd11bde1ecd74d927bcc93
PensivePasta <pensivepasta@gmail.com>
2023-10-21 13:23:06 +0100

    Add about page

---
+++ app/pages/About.js
@@ -0,0 +1,23 @@
+import Subheader from "../components/Subheader";
+import AnimatedPot from "../components/AnimatedPot";
+
+const About = () => {
+  return (
+    <div>
+      <Subheader title="About me" />
+      <AnimatedPot />
+      <p>
+        Lorem ipsum dolor sit amet consectetur. Sagittis eget hendrerit urna mus
+        massa ultrices. Pellentesque molestie vel mauris mauris rhoncus purus.
+        Tellus malesuada proin egestas in suscipit blandit suscipit ut. Nulla
+        gravida venenatis eget augue tellus. Orci volutpat congue et magnis
+        egestas massa est. Suspendisse ultricies.<br></br> Tellus malesuada
+        proin egestas in suscipit blandit suscipit ut. Nulla gravida venenatis
+        eget augue tellus. Orci volutpat congue et magnis egestas massa est.
+        Suspendisse ultricies.
+      </p>
+    </div>
+  );
+};
+
+export default About;
