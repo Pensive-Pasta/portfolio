b75750d2d256f3cb5ebf7781b5dabfa5e322aa29
PensivePasta <pensivepasta@gmail.com>
2023-10-21 13:23:33 +0100

    Add formik to contact form

---
+++ app/pages/ContactForm.js
@@ -0,0 +1,51 @@
+"use client";
+import { Formik, Field, Form } from "formik";
+import Subheader from "../components/Subheader";
+
+const ContactForm = () => {
+  return (
+    <div>
+      <Subheader title="Contact" />
+      <Formik
+        // incorrect use of initial values here
+        initialValues={{
+          name: "",
+          email: "",
+          message: "",
+        }}
+        onSubmit={(values, { resetForm }) => {
+          // Logging the form values to the console for now..
+          console.log(values);
+
+          resetForm();
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <label>
+              Name:
+              <Field type="text" name="name" placeholder="Enter Name" />
+            </label>
+            <label>
+              Email:
+              <Field type="email" name="email" placeholder="Enter Email" />
+            </label>
+            <label>
+              Message:
+              <Field
+                as="textarea"
+                name="message"
+                placeholder="Enter message e.g: 'I want to hire you!'"
+              />
+            </label>
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default ContactForm;
