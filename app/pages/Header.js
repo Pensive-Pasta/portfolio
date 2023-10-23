d965bb399778202028ed7b085d9dabc126661efd
PensivePasta <pensivepasta@gmail.com>
2023-10-23 15:51:18 +0100

    Added styling to header component.

--- app/pages/Header.js
+++ app/pages/Header.js
@@ -1,8 +1,13 @@
+import "@/styles/header.css"
+
+
 const Header = () => {
   return (
     <header>
+      <div className="h3Wrapper">
       <h1>RICKY BUTCHER</h1>
       <h3>Software Engineer</h3>
+      </div>
     </header>
   );
 };
