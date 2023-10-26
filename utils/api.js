f19908884cf3f20b48aadf912d5203a9fb714fc9
PensivePasta <pensivepasta@gmail.com>
2023-10-26 11:38:34 +0100

    Fix: Handle JSON response in contact form submission to clear form on success

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 
 export async function sendContactForm(data) {
     try {
-      const response = await fetch('http://localhost:3000/contact', {
+      const response = await fetch('http://localhost:3001/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
