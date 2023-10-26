19de219b9562c2aa4af59b31cf0dc49d48c69a3e
PensivePasta <pensivepasta@gmail.com>
2023-10-26 09:17:08 +0100

    Set up API utility for sending contact form data to the backend server.

--- app/pages/ContactForm.js
+++ app/pages/ContactForm.js
@@ -1,6 +1,7 @@
 "use client";
 import { Formik, Field, Form } from "formik";
 import Subheader from "../components/Subheader";
+import { sendContactForm } from "@/utils/api";
 
 import "@/styles/contactForm.css";
 
@@ -9,19 +10,27 @@ const ContactForm = () => {
     <div>
       <Subheader title="CONTACT" />
       <Formik
+  initialValues={{
+    name: "",
+    email: "",
+    message: "",
+  }}
+  onSubmit={async (values, { resetForm, setSubmitting }) => {
+    try {
+      
+      const response = await sendContactForm(values);
+      console.log('Contact form sent successfully:', response);
 
-        initialValues={{
-          name: "",
-          email: "",
-          message: "",
-        }}
-        onSubmit={(values, { resetForm }) => {
-          // Logging the form values to the console for now..
-          console.log(values);
-
-          resetForm();
-        }}
-      >
+      
+      resetForm();
+    } catch (error) {
+      console.error('Error sending contact form:', error);
+    } finally {
+     
+      setSubmitting(false);
+    }
+  }}
+>
         {({ isSubmitting }) => (
           <Form>
             <label>
---
+++ utils/api.js
@@ -0,0 +1,20 @@
+
+export async function sendContactForm(data) {
+    try {
+      const response = await fetch('http://localhost:3000/contact', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        throw new Error(`Network response was not ok ${response.statusText}`);
+      }
+      return response.json();
+    } catch (error) {
+      console.error('There has been a problem with your fetch operation:', error);
+      throw error;
+    }
+  }
+  
