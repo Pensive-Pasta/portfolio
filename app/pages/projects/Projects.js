729949b6997ac72aacf344be12347155a8b679e1
PensivePasta <pensivepasta@gmail.com>
2023-10-27 14:43:14 +0100

    Fix in recent update

--- app/pages/projects/Projects.js
+++ app/pages/projects/Projects.js
@@ -1,29 +1,23 @@
+"use client";
+import { useState, useEffect } from "react";
+import Project from "./Project";
+import Subheader from "../../components/Subheader";
+
 const Projects = () => {
   const [projectData, setProjectData] = useState([]);
 
   useEffect(() => {
-    const cachedData = localStorage.getItem("projectData");
-    const cachedTime = localStorage.getItem("projectDataTime");
-
-    if (cachedData && cachedTime && (Date.now() - cachedTime < 24 * 60 * 60 * 1000)) {
-      setProjectData(JSON.parse(cachedData));
-    } else {
-      fetch("https://portfolio-backend-c95e.onrender.com/projects")
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok " + response.statusText);
-          }
-          return response.json();
-        })
-        .then((data) => {
-          setProjectData(data);
-          localStorage.setItem("projectData", JSON.stringify(data));
-          localStorage.setItem("projectDataTime", Date.now());
-        })
-        .catch((error) =>
-          console.error("There was a problem with your fetch operation:", error)
-        );
-    }
+    fetch("http://localhost:3001/projects")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok " + response.statusText);
+        }
+        return response.json();
+      })
+      .then((data) => setProjectData(data))
+      .catch((error) =>
+        console.error("There was a problem with your fetch operation:", error)
+      );
   }, []);
 
   return (
