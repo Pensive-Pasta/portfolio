2f5deb30a435da70368716678013ff5fe2d31972
PensivePasta <pensivepasta@gmail.com>
2023-12-08 14:23:00 +0000

    Add loading component for project fetching.

---
+++ app/components/loading.js
@@ -0,0 +1,17 @@
+
+const Loading = () => {
+    return (
+        <div className="loading">
+
+        <div className="spinner"></div>
+        <p>
+        Waking up the backend.
+        <br />
+        Please allow a minute. 😴
+        </p>
+
+      </div>
+    );
+}
+
+export default Loading;
